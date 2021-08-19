8174923a9b6f47881f56ba81f15cddbda9e169a8
opusberedugo <46562000+opusberedugo@users.noreply.github.com>
2021-08-19 12:07:03 +0100

    Home Screen Inifinte Scroll

--- javascript/index.new.js
+++ javascript/index.new.js
@@ -1,12 +1,11 @@
-let alreadyfavSVG_HTML =
-  `<svg width="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-    <path d="M16.5 3C19.538 3 22 5.5 22 9c0 7-7.5 11-10 12.5C9.5 20 2 16 2 9c0-3.5 2.5-6 5.5-6C9.36 3 11 4 12 5c1-1 2.64-2 4.5-2z"/>
-  </svg>
-`;
+let addfavSVG_HTML =
+  `<svg width=24 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M19 14v3h3v2h-3.001L19 22h-2l-.001-3H14v-2h3v-3h2zm1.243-9.243c2.262 2.268 2.34 5.88.236 8.235l-1.42-1.418c1.331-1.524 1.261-3.914-.232-5.404-1.503-1.499-3.92-1.563-5.49-.153l-1.335 1.198-1.336-1.197c-1.575-1.412-3.991-1.35-5.494.154-1.49 1.49-1.565 3.875-.192 5.451l8.432 8.446L12 21.485 3.52 12.993c-2.104-2.356-2.025-5.974.236-8.236 2.265-2.264 5.888-2.34 8.244-.228 2.349-2.109 5.979-2.039 8.242.228z"/>
+  </svg>`;
 
 let faveSVG_HTML =
   `
-  <svg width="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <svg width="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
     <path d="M16.5 3C19.538 3 22 5.5 22 9c0 7-7.5 11-10 12.5C9.5 20 2 16 2 9c0-3.5 2.5-6 5.5-6C9.36 3 11 4 12 5c1-1 2.64-2 4.5-2z"/>
   </svg>
 `;
@@ -100,6 +99,15 @@ window.onload = () => {
   }).then((data) => {
     console.log(data.genres)
     genres = data.genres
+  }).finally(() => {
+
+    genres.forEach((v, i) => {
+      let option = document.createElement("a")
+      option.classList.add("option")
+
+      option.textContent = v.name;
+      document.querySelector(".category div.options").appendChild(option)
+    })
   })
 }
 
